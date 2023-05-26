4245b03006da4a2a421b8376b7e8e4c67d1b41cf
willylaurents <willy.laurents@bukalapak.com>
2023-05-26 22:25:05 +0700

    feat: daily_reflection_page.dart

--- lib/src/page/daily_reflection/daily_reflection_page.dart
+++ lib/src/page/daily_reflection/daily_reflection_page.dart
@@ -34,91 +34,90 @@ class DailyReflectionPage extends GetView<DailyReflectionController> {
             children: [
               SizedBox(
                 height: Get.height - 64,
-                child: Screenshot(
-                  controller: screenshotController,
-                  child: Stack(
-                    children: [
-                      _getBackground(),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
+                child: Stack(
+                  children: [
+                    Screenshot(
+                      controller: screenshotController,
+                      child: Stack(
                         children: [
-                          Padding(
-                            padding: EdgeInsets.only(
-                              top: MediaQuery.of(context).padding.top + 16,
-                              left: 16,
-                            ),
-                            child: CircularButtonComponent(
-                              icon: Iconography.close,
-                              onPressed: () {
-                                Get.back();
-                              },
-                            ),
-                          ),
-                          Expanded(
-                            child: Center(
+                          _getBackground(),
+                          Center(
+                            child: Padding(
+                              padding: const EdgeInsets.only(
+                                left: 32.0,
+                                top: 80,
+                                right: 32,
+                                bottom: 72,
+                              ),
                               child: SingleChildScrollView(
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 32.0,
-                                  ),
-                                  child: Column(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Html(
-                                        data: content.join('<br><br>'),
-                                        style: {
-                                          "body": Style(
-                                            color: ColorToken.white,
-                                            fontSize: const FontSize(20),
-                                            fontWeight: FontWeight.w700,
-                                          ),
-                                        },
-                                      ),
-                                      const SizedBox(
-                                        height: 42,
-                                      ),
-                                      Html(
-                                        data: source,
-                                        style: {
-                                          "body": Style(
-                                            color: ColorToken.white,
-                                            fontSize: const FontSize(14),
-                                            fontWeight: FontWeight.w700,
-                                            textAlign: TextAlign.center,
-                                          ),
-                                        },
-                                      ),
-                                    ],
-                                  ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Html(
+                                      data: content.join('<br><br>'),
+                                      style: {
+                                        "body": Style(
+                                          color: ColorToken.white,
+                                          fontSize: const FontSize(20),
+                                          fontWeight: FontWeight.w700,
+                                        ),
+                                      },
+                                    ),
+                                    const SizedBox(
+                                      height: 42,
+                                    ),
+                                    Html(
+                                      data: source,
+                                      style: {
+                                        "body": Style(
+                                          color: ColorToken.white,
+                                          fontSize: const FontSize(14),
+                                          fontWeight: FontWeight.w700,
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      },
+                                    ),
+                                  ],
                                 ),
                               ),
                             ),
-                          )
+                          ),
                         ],
                       ),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: Wrap(
-                            children: const [
-                              CircularButtonComponent(
-                                icon: Iconography.columns,
-                              ),
-                              SizedBox(
-                                width: 8,
-                              ),
-                              CircularButtonComponent(
-                                icon: Iconography.volumeMax,
-                              ),
-                            ],
-                          ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).padding.top + 16,
+                        left: 16,
+                      ),
+                      child: CircularButtonComponent(
+                        icon: Iconography.close,
+                        onPressed: () {
+                          Get.back();
+                        },
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Wrap(
+                          children: const [
+                            CircularButtonComponent(
+                              icon: Iconography.columns,
+                            ),
+                            SizedBox(
+                              width: 8,
+                            ),
+                            CircularButtonComponent(
+                              icon: Iconography.volumeMax,
+                            ),
+                          ],
                         ),
-                      )
-                    ],
-                  ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               Align(
@@ -169,10 +168,7 @@ class DailyReflectionPage extends GetView<DailyReflectionController> {
   Widget _getBackground() {
     return Container(
       decoration: BoxDecoration(
-        borderRadius: const BorderRadius.only(
-          bottomLeft: Radius.circular(8),
-          bottomRight: Radius.circular(8),
-        ),
+        borderRadius: BorderRadius.circular(8),
         image: DecorationImage(
           image: AssetImage(
             'asset/drawable/${controller.background}',
