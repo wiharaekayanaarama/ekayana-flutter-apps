df34003dee3eae60b1a5e6bd0be9b12ddafe6ad3
willylaurents <willy.laurents@bukalapak.com>
2023-05-27 13:07:40 +0700

    feat: fix event page

--- lib/src/page/event/event_page.dart
+++ lib/src/page/event/event_page.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ekayanaarama/ekayana.dart';
 import 'package:ekayanaarama/src/routes/route_name.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 class EventPage extends StatelessWidget {
@@ -17,19 +18,23 @@ class EventPage extends StatelessWidget {
         },
       ),
       backgroundColor: ColorToken.white,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: InComingEventComposite(
-              pageController: PageController(viewportFraction: 0.63),
-            ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              InComingEventComposite(
+                pageController: PageController(viewportFraction: 0.63),
+              ),
+              Text(
+                "Event sebelumnya",
+                style: TypographyToken.textLargeBold,
+              ),
+              _PreviousEvent(),
+            ],
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: _PreviousEvent(),
-          ),
-        ],
+        ),
       ),
     );
   }
@@ -41,81 +46,75 @@ class _PreviousEvent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
-          "Event sebelumnya",
-          style: TypographyToken.textLargeBold,
+        const SizedBox(
+          height: 8,
         ),
-        const SizedBox(height: 8,),
-        Container(
-          padding: const EdgeInsets.all(6),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            color: ColorToken.gray_0,
-          ),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: const Image(
-                  image: CachedNetworkImageProvider(
-                    'https://via.placeholder.com/225x280',
+        InkWell(
+          onTap: () => Get.toNamed(RouteName.eventDetail),
+          child: Container(
+            padding: const EdgeInsets.all(6),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              color: ColorToken.gray_0,
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: const Image(
+                    image: CachedNetworkImageProvider(
+                      'https://via.placeholder.com/225x280',
+                    ),
+                    fit: BoxFit.cover,
+                    width: 72,
+                    height: 72,
                   ),
-                  fit: BoxFit.cover,
-                  width: 72,
-                  height: 72,
                 ),
-              ),
-              const SizedBox(
-                width: 12,
-              ),
-              InkWell(
-                onTap: () {
-                  Get.toNamed(RouteName.eventDetail);
-                },
-                child: SizedBox(
-                  height: 72,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 4.0),
-                        child: Text(
-                          "Title Event",
-                          style: TypographyToken.textMediumBold,
-                        ),
+                const SizedBox(
+                  width: 12,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Title Event",
+                      style: TypographyToken.textMediumBold,
+                    ),
+                    Text(
+                      "Dari Ekayana",
+                      style: TypographyToken.textSmallRegular.apply(
+                        color: ColorToken.gray_500,
                       ),
-                      Text(
-                        "Dari Ekayana",
-                        style: TypographyToken.textSmallRegular.apply(
-                          color: ColorToken.gray_500,
+                    ),
+                    const SizedBox(
+                      height: 22,
+                    ),
+                    Wrap(
+                      children: [
+                        SvgPicture.asset(
+                          Iconography.calendar,
+                          height: 16,
+                          width: 16,
                         ),
-                      ),
-                      Expanded(
-                        child: Align(
-                          alignment: Alignment.bottomCenter,
-                          child: Padding(
-                            padding: const EdgeInsets.only(bottom: 4.0),
-                            child: TextIconComponent(
-                              text: "28 November 2023",
-                              textStyle: TypographyToken.textSmallRegular.apply(
-                                color: ColorToken.gray_500,
-                              ),
-                              iconLeft: Iconography.calendar,
-                              iconColor: ColorToken.gray_500,
-                            ),
+                        const SizedBox(
+                          width: 4,
+                        ),
+                        Text(
+                          "28 November 2023",
+                          style: TypographyToken.textSmallRegular.apply(
+                            color: ColorToken.gray_500,
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              )
-            ],
+                        )
+                      ],
+                    )
+                  ],
+                )
+              ],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
