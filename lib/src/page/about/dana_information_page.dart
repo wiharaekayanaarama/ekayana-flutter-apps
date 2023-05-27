a5c7f78aab76777bec3d848116d968cba220c1af
willylaurents <willy.laurents@bukalapak.com>
2023-05-27 11:58:53 +0700

    feat: dana_information_page.dart

--- lib/src/page/about/dana_information_page.dart
+++ lib/src/page/about/dana_information_page.dart
@@ -1,5 +1,6 @@
 import 'package:ekayanaarama/ekayana.dart';
 import 'package:ekayanaarama/src/component/button_component.dart';
+import 'package:ekayanaarama/src/component/circular_button_component.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -11,46 +12,53 @@ class DanaInformationPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: ColorToken.white,
-        appBar: NavBarComponent(
-          title: "Informasi Dana",
-          onNavigationTap: () => Get.back(),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: const [
-                Text(
-                  "Bagi Anda yang ingin berdana untuk perkembangan Buddha Dharma, Anda bisa menyalurkan dana Anda melalui transfer ke:",
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                _BankAccountComponent(
-                  accountName: 'PU Wihara Ekayana Arama',
-                  accountNumber: '3973019968',
-                ),
-                SizedBox(height: 24,),
-                _BankAccountComponent(
-                  accountName: 'Wihara Ekayana Arama',
-                  accountNumber: '1273012978',
-                ),
-              ],
-            ),
+      backgroundColor: ColorToken.white,
+      appBar: NavBarComponent(
+        title: "Informasi Dana",
+        onNavigationTap: () => Get.back(),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: const [
+              Text(
+                "Bagi Anda yang ingin berdana untuk perkembangan Buddha Dharma, Anda bisa menyalurkan dana Anda melalui transfer ke:",
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              _BankAccountComponent(
+                accountName: 'PU Wihara Ekayana Arama',
+                accountNumber: '3973019968',
+                qrisPath: 'asset/drawable/qris_pu_ekayana.png',
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              _BankAccountComponent(
+                accountName: 'Wihara Ekayana Arama',
+                accountNumber: '1273012978',
+                qrisPath: 'asset/drawable/qris_ekayana.png',
+              ),
+            ],
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
 
 class _BankAccountComponent extends StatelessWidget {
   final String accountName;
   final String accountNumber;
+  final String qrisPath;
 
   const _BankAccountComponent({
     Key? key,
     required this.accountName,
     required this.accountNumber,
+    required this.qrisPath,
   }) : super(key: key);
 
   @override
@@ -116,7 +124,32 @@ class _BankAccountComponent extends StatelessWidget {
               Expanded(
                 child: ButtonComponent(
                   text: "Lihat QRIS",
-                  onPressed: () {},
+                  onPressed: () {
+                    Get.dialog(
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 42),
+                            child: CircularButtonComponent(
+                              icon: Iconography.close,
+                              style: EkaCircularButtonStyle.white,
+                              onPressed: () {
+                                Get.back();
+                              },
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 16,
+                          ),
+                          Center(
+                            child: Image.asset(qrisPath),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
                 ),
               ),
               const SizedBox(
