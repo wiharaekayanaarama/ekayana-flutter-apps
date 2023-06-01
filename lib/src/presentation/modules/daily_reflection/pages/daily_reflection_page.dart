8ecd70d935444616bb1582f02be5cd9a317dff65
willylaurents <willy.laurents@bukalapak.com>
2023-06-02 00:39:27 +0700

    add scaffold to remove yellow line

--- lib/src/presentation/modules/daily_reflection/pages/daily_reflection_page.dart
+++ lib/src/presentation/modules/daily_reflection/pages/daily_reflection_page.dart
@@ -22,139 +22,141 @@ class DailyReflectionPage extends GetView<DailyReflectionController> {
         final Map<String, dynamic> json = snapshot.data;
         final List<String> content = json['content'].cast<String>().toList();
         final source = json['source'];
-        return Container(
-          color: ColorToken.black,
-          child: Stack(
-            children: [
-              SizedBox(
-                height: GetPlatform.isIOS ? Get.height - 80 : Get.height - 64,
-                child: Stack(
-                  children: [
-                    Screenshot(
-                      controller: screenshotController,
-                      child: Stack(
-                        children: [
-                          _getBackground(),
-                          Center(
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                left: 32.0,
-                                top: 80,
-                                right: 32,
-                                bottom: 72,
-                              ),
-                              child: SingleChildScrollView(
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    Html(
-                                      data: content.join('<br><br>'),
-                                      style: {
-                                        "body": Style(
-                                          color: ColorToken.white,
-                                          fontSize: FontSize(20),
-                                          fontWeight: FontWeight.w700,
-                                        ),
-                                      },
-                                    ),
-                                    const SizedBox(
-                                      height: 42,
-                                    ),
-                                    Html(
-                                      data: source,
-                                      style: {
-                                        "body": Style(
-                                          color: ColorToken.white,
-                                          fontSize: FontSize(14),
-                                          fontWeight: FontWeight.w700,
-                                          textAlign: TextAlign.center,
-                                        ),
-                                      },
-                                    ),
-                                  ],
+        return Scaffold(
+          body: Container(
+            color: ColorToken.black,
+            child: Stack(
+              children: [
+                SizedBox(
+                  height: GetPlatform.isIOS ? Get.height - 80 : Get.height - 64,
+                  child: Stack(
+                    children: [
+                      Screenshot(
+                        controller: screenshotController,
+                        child: Stack(
+                          children: [
+                            _getBackground(),
+                            Center(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  left: 32.0,
+                                  top: 80,
+                                  right: 32,
+                                  bottom: 72,
+                                ),
+                                child: SingleChildScrollView(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    children: [
+                                      Html(
+                                        data: content.join('<br><br>'),
+                                        style: {
+                                          "body": Style(
+                                            color: ColorToken.white,
+                                            fontSize: FontSize(20),
+                                            fontWeight: FontWeight.w700,
+                                          ),
+                                        },
+                                      ),
+                                      const SizedBox(
+                                        height: 42,
+                                      ),
+                                      Html(
+                                        data: source,
+                                        style: {
+                                          "body": Style(
+                                            color: ColorToken.white,
+                                            fontSize: FontSize(14),
+                                            fontWeight: FontWeight.w700,
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        },
+                                      ),
+                                    ],
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(
-                        top: MediaQuery.of(context).padding.top + 16,
-                        left: 16,
-                      ),
-                      child: CircularButtonComponent(
-                        icon: Iconography.close,
-                        onPressed: () {
-                          Get.back();
-                        },
-                      ),
-                    ),
-                    // Align(
-                    //   alignment: Alignment.bottomRight,
-                    //   child: Padding(
-                    //     padding: const EdgeInsets.all(16.0),
-                    //     child: Wrap(
-                    //       children: const [
-                    //         CircularButtonComponent(
-                    //           icon: Iconography.columns,
-                    //         ),
-                    //         SizedBox(
-                    //           width: 8,
-                    //         ),
-                    //         CircularButtonComponent(
-                    //           icon: Iconography.volumeMax,
-                    //         ),
-                    //       ],
-                    //     ),
-                    //   ),
-                    // ),
-                  ],
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: GetPlatform.isIOS
-                      ? const EdgeInsets.only(left: 16.0, bottom: 24, right: 16)
-                      : const EdgeInsets.symmetric(horizontal: 16.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: ButtonComponent(
-                          text: "Share",
-                          iconLeft: Iconography.share_06,
-                          onPressed: () async {
-                            screenshotController.capture().then((value) async {
-                              File file =
-                                  await controller.fileFromUint8List(value!);
-                              Share.shareFiles([file.path]);
-                            });
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: MediaQuery.of(context).padding.top + 16,
+                          left: 16,
+                        ),
+                        child: CircularButtonComponent(
+                          icon: Iconography.close,
+                          onPressed: () {
+                            Get.back();
                           },
                         ),
                       ),
-                      const SizedBox(
-                        width: 32,
-                      ),
-                      Expanded(
-                        child: ButtonComponent(
-                          text: "Unduh",
-                          iconLeft: Iconography.download_01,
-                          style: EkaButtonStyle.naked,
-                          onPressed: () async {
-                            screenshotController.capture().then((value) async {
-                              controller.saveToGallery(value!);
-                            });
-                          },
-                        ),
-                      ),
+                      // Align(
+                      //   alignment: Alignment.bottomRight,
+                      //   child: Padding(
+                      //     padding: const EdgeInsets.all(16.0),
+                      //     child: Wrap(
+                      //       children: const [
+                      //         CircularButtonComponent(
+                      //           icon: Iconography.columns,
+                      //         ),
+                      //         SizedBox(
+                      //           width: 8,
+                      //         ),
+                      //         CircularButtonComponent(
+                      //           icon: Iconography.volumeMax,
+                      //         ),
+                      //       ],
+                      //     ),
+                      //   ),
+                      // ),
                     ],
                   ),
                 ),
-              ),
-            ],
+                Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Padding(
+                    padding: GetPlatform.isIOS
+                        ? const EdgeInsets.only(left: 16.0, bottom: 24, right: 16)
+                        : const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ButtonComponent(
+                            text: "Share",
+                            iconLeft: Iconography.share_06,
+                            onPressed: () async {
+                              screenshotController.capture().then((value) async {
+                                File file =
+                                    await controller.fileFromUint8List(value!);
+                                Share.shareFiles([file.path]);
+                              });
+                            },
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 32,
+                        ),
+                        Expanded(
+                          child: ButtonComponent(
+                            text: "Unduh",
+                            iconLeft: Iconography.download_01,
+                            style: EkaButtonStyle.naked,
+                            onPressed: () async {
+                              screenshotController.capture().then((value) async {
+                                controller.saveToGallery(value!);
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         );
       },
