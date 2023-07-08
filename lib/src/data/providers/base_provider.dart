49cd67a346d2f26ca7afa05d7ba7ac4fcaeec139
willylaurents <willy.laurents@bukalapak.com>
2023-07-08 12:38:48 +0700

    * add headers to api call

--- lib/src/data/providers/base_provider.dart
+++ lib/src/data/providers/base_provider.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:ekayanaarama/src/utils/app_info.dart';
 import 'package:ekayanaarama/src/utils/device_info.dart';
 import 'package:get/get_connect/connect.dart';
 
@@ -6,20 +9,21 @@ class BaseProvider extends GetConnect {
   void onInit() {
     httpClient.baseUrl = 'http://api.ekayana.or.id/gateway';
     httpClient.timeout = const Duration(minutes: 1);
-    // httpClient.addAuthenticator<dynamic>((request) async {
-    //   request.headers.addAll(getHeaders());
-    //   return request;
-    // });
+    httpClient.addAuthenticator<dynamic>((request) async {
+      request.headers.addAll(getHeaders());
+      return request;
+    });
 
     super.onInit();
   }
 
   Map<String, String> getHeaders() {
+    final platform = Platform.isAndroid ? "ANDROID" : "IOS";
     return {
-      "User-Agent": "",
+      "User-Agent": "Ekayana-$platform-Version",
       "Accept": "application/json",
-      "Eka-Version": "",
-      "Eka-Platform": "",
+      "Eka-Version": AppInfo.appVersion,
+      "Eka-Platform": platform,
       "Eka-Device-ID": DeviceInfo.deviceId,
       "Eka-Device-Model": DeviceInfo.model,
       "Eka-Device-OS": DeviceInfo.os,
